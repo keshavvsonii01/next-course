a3c7c3bb5c0fb6c9b4f5365bc976094900a0fe98
Keshav Soni <keshavvsonii01@gmail.com>
2024-09-29 18:10:35 +0530

    Day2Next

--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
+'use client'
+import Link from 'next/link';
 import React from 'react'
-
+import { useRouter } from 'next/navigation'
 function Home() {
+
+  const router = useRouter();
+  const navigate = (page) => {
+    router.push(page)
+  }
+  
+
   return (
-    <div>Home</div>
+    <>
+    <h1>Welcome to My Home Page</h1>
+    </>
   )
 }
 
-export default Home
+export default Home 
---
+++ app/users/[user]/page.js
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+function User({params}) {
+  return (
+    <div>Info abt {params.user}</div>
+  )
+}
+
+export default User
